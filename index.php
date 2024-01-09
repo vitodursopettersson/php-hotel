909daae04faf27eaca5943196d1d54586bf84037
vitodursopettersson <vitodurso98@gmail.com>
2024-01-09 17:03:42 +0100

    parking filter

--- index.php
+++ index.php
@@ -54,23 +54,81 @@ $hotels = [
 
 <body>
 
-    <!-- Lista Hotel -->
+    <!-- Titolo -->
     <div class="col-8 mx-auto mt-4">
+        <h1>Php Hotel</h1>
+    </div>
 
+    <!-- Form Ricerca -->
+    <div class="col-8 mx-auto mt-4">
+        <h6>Filter</h6>
+        <form class="d-flex align-items-baseline" method="GET">
+
+            <div class="mr-5">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="parking" name="parking">
+                    <label class="form-check-label" for="parking">
+                        parking
+                    </label>
+                </div>
+            </div>
+
+            <div class="mr-5">
+                <label class="visually-hidden" for="minimum-vote">minimum vote</label>
+                <select class="form-select" id="minimum-vote" name="minimum-vote">
+                    <option selected value="">choose...</option>
+                    <option value="1">one</option>
+                    <option value="2">two</option>
+                    <option value="3">three</option>
+                    <option value="4">four</option>
+                    <option value="5">five</option>
+                </select>
+            </div>
+
+            <div class="mr-5">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+
+        </form>
+    </div>
+
+    <?php
+    $parking = $_GET["parking"];
+    $minimum_vote = $_GET["minimum-vote"];
+    $filter_hotels = [];
+
+    foreach ($hotels as $hotel) {
+        if ($parking == false) {
+            $filter_hotels[] = $hotel;
+        } else {
+            if ($hotel['parking'] == $parking) {
+                $filter_hotels[] = $hotel;
+            };
+        }
+    };
+
+
+    ?>
+
+    <!-- Lista Hotel -->
+
+    <div class="col-8 mx-auto mt-4">
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <?php
-                    foreach ($hotels[0] as $title => $info) {
-                        echo "<th scope=\"col\">$title</th>";
-                    };
-                    ?>
+                    <th scope="col">name</th>
+                    <th scope="col">description</th>
+                    <th scope="col">parking</th>
+                    <th scope="col">vote</th>
+                    <th scope="col">distance to center</th>
+
                 </tr>
             </thead>
             <tbody>
                 <?php
-                foreach ($hotels as $index_hotel => $hotel) {
+
+                foreach ($filter_hotels as $index_hotel => $hotel) {
                     echo "<tr>";
                     echo "<th scope=\"row\">$index_hotel</th>";
 
